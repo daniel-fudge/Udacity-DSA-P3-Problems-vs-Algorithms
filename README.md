830311dda5b58389f29554a071aad107bfb1d632
Daniel Fudge <daniel.fudge1977@gmail.com>
2022-05-27 13:25:31 -0400

    Problem 7 complete but needs more testing.

--- README.md
+++ README.md
@@ -30,6 +30,26 @@ See [explanation_6.md](explanation_6.md) for more details and [problem_6.py](pro
 ### Problem 7 - Request Routing in a Web Server with a Trie
 See [explanation_7.md](explanation_7.md) for more details and [problem_7.py](problem_7.py) for the solution. 
 
+## Setup Python Virtual Environment (VENV)
+Problem 5 uses a Jupyter Notebook, so we require a venv with Jupyter installed.
+
+### Install Base Python v3.10 Interpreter
+To start we assume that the base Python v3.10 interpreter is installed and is in the Path. If not, please install the 
+v3.10 interpreter from [here](https://www.python.org/).   
+
+### Create Virtual Environment
+If you are not familiar with Python Virtual Environments, please see the tutorial [here](https://docs.python.org/3/tutorial/venv.html). 
+The Powershell commands are executed from within the repo's root folder, and assumes Python is installed in 
+`C:\Python3.10.4`.  Linux and other shell commands are very similar.
+```shell
+C:\Python3.10.4\python.exe -m venv venv
+.\venv\Scripts\activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+```
+
 ## References
 https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256
 https://review.udacity.com/#!/rubrics/2498/view
+https://www.python.org/
+https://docs.python.org/3/tutorial/venv.html
--- problem_7.py
+++ problem_7.py
@@ -5,83 +5,329 @@ import random
 from time import time
 
 
-# A RouteTrie will store our routes and their associated handlers
-class RouteTrie:
-    def __init__(self, ...):
-        # Initialize the trie with an root node and a handler, this is the root path or home page node
-
-    def insert(self, ...):
-        # Similar to our previous example you will want to recursively add nodes
-        # Make sure you assign the handler to only the leaf (deepest) node of this path
-
-    def find(self, ...):
-        # Starting at the root, navigate the Trie to find a match for this path
-        # Return the handler for a match, or None for no match
-
-# A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
 class RouteTrieNode:
-    def __init__(self, ...):
-        # Initialize the node with children as before, plus a handler
+    """A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler."""
 
-    def insert(self, ...):
-        # Insert the node as before
+    def __init__(self, path: str):
+        self.path = path
+        self.handler = None
+        self.children = {}
 
-        # The Router class will wrap the Trie and handle
-        class Router:
-            def __init__(self, ...):
-
-            # Create a new RouteTrie for holding our routes
-            # You could also add a handler for 404 page not found responses as well!
-
-            def add_handler(self, ...):
-
-            # Add a handler for a path
-            # You will need to split the path and pass the pass parts
-            # as a list to the RouteTrie
-
-            def lookup(self, ...):
-
-            # lookup path (by parts) and return the associated handler
-            # you can return None if it's not found or
-            # return the "not found" handler if you added one
-            # bonus points if a path works with and without a trailing slash
-            # e.g. /about and /about/ both return the /about handler
-
-            def split_path(self, ...):
-        # you need to split the path into parts for
-        # both the add_handler and loopup functions,
-        # so it should be placed in a function here
+    def insert(self, path: str):
+        if path not in self.children.keys():
+            self.children[path] = RouteTrieNode(path)
 
 
-def given_tests():
+class RouteTrie:
+    """A RouteTrie will store our routes and their associated handlers"""
+
+    def __init__(self, root_handler: str):
+        self.root = RouteTrieNode(path="")
+        self.root.handler = root_handler
+
+    def insert(self, full_path: str, handler: str):
+        """Inserts the given full path into the Route Trie.
+
+        Args:
+            full_path (str): The full path to insert.
+            handler (str): The handler to add to the leaf node.
+
+        Raises:
+            AttributeError: If the given path has no non-slash characters.
+        """
+
+        # Remove leading and trailing  "/" from the given path
+        full_path = full_path.strip("/")
+
+        # Check for an empty full path
+        if len(full_path) == 0:
+            raise AttributeError("Given path has no non-slash characters.")
+
+        # Loop over all the paths between the "/" characters
+        node = self.root
+        for path in full_path.split('/'):
+            node.insert(path=path)
+            node = node.children[path]
+        node.handler = handler
+
+    def find(self, full_path: str):
+        """Starting at the root, find match for given full path and return the matching handler or None for no match.
+
+        Args:
+            full_path (str): The full path we need to match.
+
+        Returns:
+            str | None: The handle of the matching path or None if not found
+        """
+
+        # Remove leading and trailing  "/" from the given path
+        full_path = full_path.strip("/")
+
+        # Check for an empty full path, which matches the root
+        if len(full_path) == 0:
+            return self.root.handler
+
+        node = self.root
+        for path in full_path.split('/'):
+            if path not in node.children.keys():
+                return None
+            node = node.children[path]
+        return node.handler
+
+
+class Router:
+    """The HTTP Router class."""
+    def __init__(self, root_handler: str, error_handler: str):
+        """The object instantiation method.
+
+        Args:
+            root_handler (str): The handler for the root of the object.
+            error_handler (str): The handler returned when a route is not found (404 error).
+
+        Raises:
+            AttributeError: If either argument is not a string
+        """
+
+        # Check arguments
+        if not isinstance(root_handler, str):
+            raise AttributeError("The root_handler must be a string.")
+        if not isinstance(error_handler, str):
+            raise AttributeError("The error_handler must be a string.")
+
+        self.trie = RouteTrie(root_handler=root_handler)
+        self.error_handler = error_handler
+
+    def add_handler(self, full_path: str, handler: str):
+        """Adds the given handler to the node at the end of the full path.
+
+        Args:
+            full_path (str): The full path to insert.
+            handler (str): The handler to add to the leaf node.
+
+        Raises:
+            AttributeError: If either argument is not a string
+            AttributeError: If the given path has no non-slash characters.
+        """
+        # Check arguments
+        if not isinstance(full_path, str):
+            raise AttributeError("The full_path must be a string.")
+        if not isinstance(handler, str):
+            raise AttributeError("The handler must be a string.")
+        if len(full_path.replace("/", "")) == 0:
+            raise AttributeError("The full_path has no non-slash characters.")
+
+        self.trie.insert(full_path=full_path, handler=handler)
+
+    def lookup(self, full_path: str):
+        """Return handler for given full path or the error handler for no match.
+
+        Args:
+            full_path (str): The full path we need to match.
+
+        Returns:
+            str | None: The handle of the matching path or None if not found
+
+        Raises:
+            AttributeError: If argument is not a string
+        """
+
+        # Check arguments
+        if not isinstance(full_path, str):
+            raise AttributeError("The full_path must be a string.")
+
+        handler = self.trie.find(full_path=full_path)
+        if handler is None:
+            return self.error_handler
+        return handler
+
+
+def given_tests() -> int:
+    """Runs the tests defined by Udacity.
+
+    Returns:
+        int: The number of failed tests.
+    """
+
     # create the router and add a route
     router = Router("root handler", "not found handler")  # remove the 'not found handler' if you did not implement this
     router.add_handler("/home/about", "about handler")  # add a route
 
     # some lookups with the expected output
+    print("\nGiven tests:")
+    test = 0
+    n_errors = 0
+    for path, expected in [("/", 'root handler'), ("/home", 'not found handler'), ("/home/about", 'about handler'),
+                           ("/home/about/", 'about handler'), ("/home/about/me", 'not found handler')]:
+        actual = router.lookup(full_path=path)
+        test += 1
+        if actual == expected:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Test {test} failed: path = {path}, actual = {actual}, expected = {expected}.")
+            n_errors += 1
+    """
+    These are actual tests in Udacity before reformatting
     print(router.lookup("/"))  # should print 'root handler'
     print(router.lookup("/home"))  # should print 'not found handler' or None if you did not implement one
     print(router.lookup("/home/about"))  # should print 'about handler'
     print(router.lookup("/home/about/"))  # should print 'about handler' or None if you did not handle trailing slashes
     print(router.lookup("/home/about/me"))  # should print 'not found handler' or None if you did not implement one
+    """
+
+    return n_errors
+
+
+def test_handler_not_found() -> int:
+    """Test the handler not found capability.
+
+    Returns:
+        int: The number of errors
+    """
+    test = 0
+    n_errors = 0
+    good_handler = "good_handler"
+    good_router = Router(root_handler=good_handler, error_handler=good_handler)
+    for path in [f"{'/'*i}some/middle/content{'/'*j}" for i in range(3) for j in range(3)]:
+        test += 1
+        good_router.add_handler(full_path=path, handler=good_handler)
+        handler = good_router.lookup(full_path=path)
+        if handler == good_handler:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Test {test} failed: path = {path}, actual handler = {handler}, expected = {good_handler}.")
+            n_errors += 1
+
+    return n_errors
+
+
+def test_invalid_arguments() -> int:
+    """Test the top level methods with invalid arguments.
+
+    Returns:
+        int: The number of errors
+    """
+    test = 0
+    n_errors = 0
+    bad_path = "///"
+    good_handler = "good_handler"
+    good_path = "/some/good/path"
+    good_router = Router(root_handler=good_handler, error_handler=good_handler)
+    for arg in [3.5, 2, [], None]:
+        test += 1
+        try:
+            # noinspection PyTypeChecker
+            Router(root_handler=arg, error_handler=good_handler)
+        except AttributeError:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Error test {test}: expected an AttributeError exception.")
+            n_errors += 1
+
+        test += 1
+        try:
+            # noinspection PyTypeChecker
+            Router(root_handler=good_handler, error_handler=arg)
+        except AttributeError:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Error test {test}: expected an AttributeError exception.")
+            n_errors += 1
+
+        test += 1
+        try:
+            # noinspection PyTypeChecker
+            good_router.add_handler(full_path=arg, handler=good_handler)
+        except AttributeError:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Error test {test}: expected an AttributeError exception.")
+            n_errors += 1
+
+        test += 1
+        try:
+            # noinspection PyTypeChecker
+            good_router.add_handler(full_path=good_path, handler=arg)
+        except AttributeError:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Error test {test}: expected an AttributeError exception.")
+            n_errors += 1
+
+        test += 1
+        try:
+            # noinspection PyTypeChecker
+            good_router.lookup(full_path=arg)
+        except AttributeError:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Error test {test}: expected an AttributeError exception.")
+            n_errors += 1
+
+    test += 1
+    try:
+        # noinspection PyTypeChecker
+        good_router.add_handler(full_path=bad_path, handler=good_handler)
+    except AttributeError:
+        print(f"Test {test} passed.")
+    else:
+        print(f"Error test {test}: expected an AttributeError exception.")
+        n_errors += 1
+
+    return n_errors
+
+
+def test_slashes() -> int:
+    """Test multiple leading and trailing edge slashes.
+
+    Returns:
+        int: The number of errors
+    """
+    test = 0
+    n_errors = 0
+    good_handler = "good_handler"
+    good_router = Router(root_handler=good_handler, error_handler=good_handler)
+    for path in [f"{'/'*i}some/middle/content{'/'*j}" for i in range(3) for j in range(3)]:
+        test += 1
+        good_router.add_handler(full_path=path, handler=good_handler)
+        handler = good_router.lookup(full_path=path)
+        if handler == good_handler:
+            print(f"Test {test} passed.")
+        else:
+            print(f"Test {test} failed: path = {path}, actual handler = {handler}, expected = {good_handler}.")
+            n_errors += 1
+
+    return n_errors
 
 
 # noinspection PyBroadException
-def user_tests():
+def user_tests() -> int:
     """Runs the user tests."""
 
-    # Set some testing constants
     n_errors = 0
 
+    # Test set 1 - Invalid arguments
+    print("\nUser test set 1 - Invalid arguments.")
+    n_errors += test_invalid_arguments()
 
-    print("\n*******************")
-    if n_errors > 0:
-        raise RuntimeError(f"BOO HOO, {n_errors} errors detected.\n")
-    else:
-        print("WOO HOO, No errors detected.\n")
+    # Test "not found handler" (BONUS POINTS)
+    print("\nUser test set 2 - 'Not found handler' (MORE BONUS POINTS).")
+    n_errors += test_handler_not_found()
+
+    # Test multiple leading and trailing "/" (MORE BONUS POINTS)
+    print("\nUser test set 3 - Leading and training '/' (MORE BONUS POINTS).")
+    n_errors += test_slashes()
+
+    return n_errors
 
 
 # **********************************************************
 if __name__ == '__main__':
-    given_tests()
-    user_tests()
+    n_total_errors = 0
+    n_total_errors += given_tests()
+    n_total_errors += user_tests()
+
+    print("\n*******************")
+    if n_total_errors > 0:
+        raise RuntimeError(f"BOO HOO, {n_total_errors} errors detected.\n")
+    else:
+        print("WOO HOO, No errors detected.\n")
